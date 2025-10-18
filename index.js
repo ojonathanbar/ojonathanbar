222f2264df170de6ff2f62a4db6da07cd224bdda
ojonathanbar <jbarrosodesigner@gmail.com>
2025-10-18 17:47:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import Portfolio from '../components/Portfolio'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Footer />
+    </>
+  )
+}
